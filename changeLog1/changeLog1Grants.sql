4f2a6ba473254f09e6e58c8182bdb2b1ee11a1c1
Dave Steinich <drsteini@usgs.gov>
2014-03-13 15:26:40 +0000

    WQP-246 - nemi urls

--- changeLog1/changeLog1Grants.sql
+++ changeLog1/changeLog1Grants.sql
@@ -5,3 +5,7 @@
 --changeset drsteini:1SchemaGrantsAA
 grant select on wqp_nemi_nwis_crosswalk to nwis_ws_star;
 --rollback revoke select on wqp_nemi_nwis_crosswalk from nwis_ws_star;
+
+--changeset drsteini:1SchemaGrantsAB
+grant select on nwis_method_codes to wqp_user;
+--rollback revoke select on nwis_method_codes from wqp_user;
