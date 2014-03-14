f78c584e7af41ff260c17fe0c9747935219a9400
Dave Steinich <drsteini@usgs.gov>
2014-03-14 20:54:39 +0000

    incremental liquibase script

--- changeLog1/changeLog1Grants.sql
+++ changeLog1/changeLog1Grants.sql
@@ -9,3 +9,7 @@ grant select on wqp_nemi_nwis_crosswalk to nwis_ws_star;
 --changeset drsteini:1SchemaGrantsAB
 grant select on nwis_method_codes to wqp_user;
 --rollback revoke select on nwis_method_codes from wqp_user;
+
+--changeset drsteini:1SchemaGrantsAC
+grant select, insert on web_service_log to wqp_user;
+--rollback revoke select, insert on web_service_log from wqp_user;
