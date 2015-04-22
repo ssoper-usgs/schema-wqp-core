916e57fcbeee1c7035e5e6f2de334bb38f7fba2a
David Steinich <drsteini@usgs.gov>
2015-04-22 09:21:19 -0500

    misc changes

--- denormalized-oracle/changeLog1/common/synonyms.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---This is for the wqp_core schema
-
---changeset drsteini:1CommonSynonymsAA
-create synonym public_srsnames for public_srsnames_00000;
---rollback drop synonym public_srsnames;
--- denormalized-oracle/changeLog1/storet/tables.sql
+++ denormalized-oracle/changeLog1/storet/tables.sql
@@ -573,3 +573,16 @@ create table result_no_source_00000
 ,analysis_prep_date_tx          varchar2(4000 char)
 ) parallel 4 compress pctfree 0 nologging cache;
 --rollback drop table result_no_source_00000 cascade constraints purge;
+
+--changeset drsteini:1StoretTablesAT
+create table wqp_nemi_epa_crosswalk
+(wqp_source						varchar2(6 char)
+,analytical_procedure_source	varchar2(256 char)
+,analytical_procedure_id		varchar2(256 char)
+,source_method_identifier		varchar2(30 char)
+,method_id						number
+,method_source					varchar2(20 char)
+,method_type					varchar2(100 char)
+,nemi_url						varchar2(4000 char)
+) parallel 4 compress pctfree 0 nologging;
+--rollback drop table wqp_nemi_epa_crosswalk cascade constraints purge;
