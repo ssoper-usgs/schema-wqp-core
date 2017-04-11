e9a1a9461a107b45c19e17b9f1a2616ac7ae7abb
Steinich, Dave R. <drsteini@usgs.gov>
2017-04-11 08:25:20 -0500

    Should subpartition r_detect_qnt_lmt

--- changeLogActivity/biodata/tables.sql
+++ changeLogActivity/biodata/tables.sql
@@ -556,5 +556,41 @@ create table r_detect_qnt_lmt_swap_biodata
 ,country_code                   varchar2(9 char) generated always as (regexp_substr (governmental_unit_code,'[^:]+'))
 ,state_code                     varchar2(9 char) generated always as (regexp_substr (governmental_unit_code,'[^:]+:[^:]+'))
 ,county_code                    varchar2(9 char) generated always as (regexp_substr (governmental_unit_code,'[^:]+:[^:]+:[^:]+'))
-) parallel 4 compress pctfree 0 nologging cache;
+) parallel 4 compress pctfree 0 nologging cache
+partition by range (event_date)
+(partition rdtct_qnt_lmt_biodata_p_1990 values less than (to_date('01-JAN-1990', 'DD-MON-YYYY')) tablespace result1,
+ partition rdtct_qnt_lmt_biodata_y_1990 values less than (to_date('01-JAN-1991', 'DD-MON-YYYY')) tablespace result2,
+ partition rdtct_qnt_lmt_biodata_y_1991 values less than (to_date('01-JAN-1992', 'DD-MON-YYYY')) tablespace result3,
+ partition rdtct_qnt_lmt_biodata_y_1992 values less than (to_date('01-JAN-1993', 'DD-MON-YYYY')) tablespace result4,
+ partition rdtct_qnt_lmt_biodata_y_1993 values less than (to_date('01-JAN-1994', 'DD-MON-YYYY')) tablespace result1,
+ partition rdtct_qnt_lmt_biodata_y_1994 values less than (to_date('01-JAN-1995', 'DD-MON-YYYY')) tablespace result2,
+ partition rdtct_qnt_lmt_biodata_y_1995 values less than (to_date('01-JAN-1996', 'DD-MON-YYYY')) tablespace result3,
+ partition rdtct_qnt_lmt_biodata_y_1996 values less than (to_date('01-JAN-1997', 'DD-MON-YYYY')) tablespace result4,
+ partition rdtct_qnt_lmt_biodata_y_1997 values less than (to_date('01-JAN-1998', 'DD-MON-YYYY')) tablespace result1,
+ partition rdtct_qnt_lmt_biodata_y_1998 values less than (to_date('01-JAN-1999', 'DD-MON-YYYY')) tablespace result2,
+ partition rdtct_qnt_lmt_biodata_y_1999 values less than (to_date('01-JAN-2000', 'DD-MON-YYYY')) tablespace result3,
+ partition rdtct_qnt_lmt_biodata_y_2000 values less than (to_date('01-JAN-2001', 'DD-MON-YYYY')) tablespace result4,
+ partition rdtct_qnt_lmt_biodata_y_2001 values less than (to_date('01-JAN-2002', 'DD-MON-YYYY')) tablespace result1,
+ partition rdtct_qnt_lmt_biodata_y_2002 values less than (to_date('01-JAN-2003', 'DD-MON-YYYY')) tablespace result2,
+ partition rdtct_qnt_lmt_biodata_y_2003 values less than (to_date('01-JAN-2004', 'DD-MON-YYYY')) tablespace result3,
+ partition rdtct_qnt_lmt_biodata_y_2004 values less than (to_date('01-JAN-2005', 'DD-MON-YYYY')) tablespace result4,
+ partition rdtct_qnt_lmt_biodata_y_2005 values less than (to_date('01-JAN-2006', 'DD-MON-YYYY')) tablespace result1,
+ partition rdtct_qnt_lmt_biodata_y_2006 values less than (to_date('01-JAN-2007', 'DD-MON-YYYY')) tablespace result2,
+ partition rdtct_qnt_lmt_biodata_y_2007 values less than (to_date('01-JAN-2008', 'DD-MON-YYYY')) tablespace result3,
+ partition rdtct_qnt_lmt_biodata_y_2008 values less than (to_date('01-JAN-2009', 'DD-MON-YYYY')) tablespace result4,
+ partition rdtct_qnt_lmt_biodata_y_2009 values less than (to_date('01-JAN-2010', 'DD-MON-YYYY')) tablespace result1,
+ partition rdtct_qnt_lmt_biodata_y_2010 values less than (to_date('01-JAN-2011', 'DD-MON-YYYY')) tablespace result2,
+ partition rdtct_qnt_lmt_biodata_y_2011 values less than (to_date('01-JAN-2012', 'DD-MON-YYYY')) tablespace result3,
+ partition rdtct_qnt_lmt_biodata_y_2012 values less than (to_date('01-JAN-2013', 'DD-MON-YYYY')) tablespace result4,
+ partition rdtct_qnt_lmt_biodata_y_2013 values less than (to_date('01-JAN-2014', 'DD-MON-YYYY')) tablespace result1,
+ partition rdtct_qnt_lmt_biodata_y_2014 values less than (to_date('01-JAN-2015', 'DD-MON-YYYY')) tablespace result2,
+ partition rdtct_qnt_lmt_biodata_y_2015 values less than (to_date('01-JAN-2016', 'DD-MON-YYYY')) tablespace result3,
+ partition rdtct_qnt_lmt_biodata_y_2016 values less than (to_date('01-JAN-2017', 'DD-MON-YYYY')) tablespace result4,
+ partition rdtct_qnt_lmt_biodata_y_2017 values less than (to_date('01-JAN-2018', 'DD-MON-YYYY')) tablespace result2,
+ partition rdtct_qnt_lmt_biodata_y_2018 values less than (to_date('01-JAN-2019', 'DD-MON-YYYY')) tablespace result3,
+ partition rdtct_qnt_lmt_biodata_y_2019 values less than (to_date('01-JAN-2020', 'DD-MON-YYYY')) tablespace result4,
+ partition rdtct_qnt_lmt_biodata_y_2020 values less than (to_date('01-JAN-2021', 'DD-MON-YYYY')) tablespace result1,
+ partition rdtct_qnt_lmt_biodata_y_maxx values less than (maxvalue) tablespace result2
+);
+;
 --rollback drop table r_detect_qnt_lmt_swap_biodata cascade constraints purge;
--- changeLogActivity/stewards/indexes.sql
+++ changeLogActivity/stewards/indexes.sql
@@ -207,143 +207,143 @@ create bitmap index as_stewards_station on activity_sum_swap_stewards(station_id
 --changeset drsteini:WQP-1033.create.rdql_stewards_activity
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_ACTIVITY';
-create bitmap index rdql_stewards_activity on r_detect_qnt_lmt_swap_stewards(activity) parallel 4 nologging;
+create bitmap index rdql_stewards_activity on r_detect_qnt_lmt_swap_stewards(activity) local parallel 4 nologging;
 --rollback drop index rdql_stewards_activity;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_analytical
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_ANALYTICAL';
-create bitmap index rdql_stewards_analytical on r_detect_qnt_lmt_swap_stewards(analytical_method) parallel 4 nologging;
+create bitmap index rdql_stewards_analytical on r_detect_qnt_lmt_swap_stewards(analytical_method) local parallel 4 nologging;
 --rollback drop index rdql_stewards_analytical;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_assemblage
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_ASSEMBLAGE';
-create bitmap index rdql_stewards_assemblage on r_detect_qnt_lmt_swap_stewards(assemblage_sampled_name) parallel 4 nologging;
+create bitmap index rdql_stewards_assemblage on r_detect_qnt_lmt_swap_stewards(assemblage_sampled_name) local parallel 4 nologging;
 --rollback drop index rdql_stewards_assemblage;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_char_name
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_CHAR_NAME';
-create bitmap index rdql_stewards_char_name on r_detect_qnt_lmt_swap_stewards(characteristic_name) parallel 4 nologging;
+create bitmap index rdql_stewards_char_name on r_detect_qnt_lmt_swap_stewards(characteristic_name) local parallel 4 nologging;
 --rollback drop index rdql_stewards_char_name;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_char_type
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_CHAR_TYPE';
-create bitmap index rdql_stewards_char_type on r_detect_qnt_lmt_swap_stewards(characteristic_type) parallel 4 nologging;
+create bitmap index rdql_stewards_char_type on r_detect_qnt_lmt_swap_stewards(characteristic_type) local parallel 4 nologging;
 --rollback drop index rdql_stewards_char_type;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_country
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_COUNTRY';
-create bitmap index rdql_stewards_country on r_detect_qnt_lmt_swap_stewards(country_code) parallel 4 nologging;
+create bitmap index rdql_stewards_country on r_detect_qnt_lmt_swap_stewards(country_code) local parallel 4 nologging;
 --rollback drop index rdql_stewards_country;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_county
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_COUNTY';
-create bitmap index rdql_stewards_county on r_detect_qnt_lmt_swap_stewards(county_code) parallel 4 nologging;
+create bitmap index rdql_stewards_county on r_detect_qnt_lmt_swap_stewards(county_code) local parallel 4 nologging;
 --rollback drop index rdql_stewards_county;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_event_date
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_EVENT_DATE';
-create bitmap index rdql_stewards_event_date on r_detect_qnt_lmt_swap_stewards(event_date) parallel 4 nologging;
+create bitmap index rdql_stewards_event_date on r_detect_qnt_lmt_swap_stewards(event_date) local parallel 4 nologging;
 --rollback drop index rdql_stewards_event_date;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_huc2
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_HUC2';
-create bitmap index rdql_stewards_huc2 on r_detect_qnt_lmt_swap_stewards(huc_2) parallel 4 nologging;
+create bitmap index rdql_stewards_huc2 on r_detect_qnt_lmt_swap_stewards(huc_2) local parallel 4 nologging;
 --rollback drop index rdql_stewards_huc2;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_huc4
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_HUC4';
-create bitmap index rdql_stewards_huc4 on r_detect_qnt_lmt_swap_stewards(huc_4) parallel 4 nologging;
+create bitmap index rdql_stewards_huc4 on r_detect_qnt_lmt_swap_stewards(huc_4) local parallel 4 nologging;
 --rollback drop index rdql_stewards_huc4;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_huc6
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_HUC6';
-create bitmap index rdql_stewards_huc6 on r_detect_qnt_lmt_swap_stewards(huc_6) parallel 4 nologging;
+create bitmap index rdql_stewards_huc6 on r_detect_qnt_lmt_swap_stewards(huc_6) local parallel 4 nologging;
 --rollback drop index rdql_stewards_huc6;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_huc8
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_HUC8';
-create bitmap index rdql_stewards_huc8 on r_detect_qnt_lmt_swap_stewards(huc_8) parallel 4 nologging;
+create bitmap index rdql_stewards_huc8 on r_detect_qnt_lmt_swap_stewards(huc_8) local parallel 4 nologging;
 --rollback drop index rdql_stewards_huc8;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_huc10
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_HUC10';
-create bitmap index rdql_stewards_huc10 on r_detect_qnt_lmt_swap_stewards(huc_10) parallel 4 nologging;
+create bitmap index rdql_stewards_huc10 on r_detect_qnt_lmt_swap_stewards(huc_10) local parallel 4 nologging;
 --rollback drop index rdql_stewards_huc10;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_huc12
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_HUC12';
-create bitmap index rdql_stewards_huc12 on r_detect_qnt_lmt_swap_stewards(HUC_12) parallel 4 nologging;
+create bitmap index rdql_stewards_huc12 on r_detect_qnt_lmt_swap_stewards(HUC_12) local parallel 4 nologging;
 --rollback drop index rdql_stewards_huc12;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_organization
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_ORGANIZATION';
-create bitmap index rdql_stewards_organization on r_detect_qnt_lmt_swap_stewards(organization) parallel 4 nologging;
+create bitmap index rdql_stewards_organization on r_detect_qnt_lmt_swap_stewards(organization) local parallel 4 nologging;
 --rollback drop index rdql_stewards_organization;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_p_code
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_P_CODE';
-create bitmap index rdql_stewards_p_code on r_detect_qnt_lmt_swap_stewards(p_code) parallel 4 nologging;
+create bitmap index rdql_stewards_p_code on r_detect_qnt_lmt_swap_stewards(p_code) local parallel 4 nologging;
 --rollback drop index rdql_stewards_p_code;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_project
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_PROJECT';
-create bitmap index rdql_stewards_project on r_detect_qnt_lmt_swap_stewards(project_id) parallel 4 nologging;
+create bitmap index rdql_stewards_project on r_detect_qnt_lmt_swap_stewards(project_id) local parallel 4 nologging;
 --rollback drop index rdql_stewards_project;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_result
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_RESULT';
-create bitmap index rdql_stewards_result on r_detect_qnt_lmt_swap_stewards(result_id) parallel 4 nologging;
+create bitmap index rdql_stewards_result on r_detect_qnt_lmt_swap_stewards(result_id) local parallel 4 nologging;
 --rollback drop index rdql_stewards_result;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_sample_media
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_SAMPLE_MEDIA';
-create bitmap index rdql_stewards_sample_media on r_detect_qnt_lmt_swap_stewards(sample_media) parallel 4 nologging;
+create bitmap index rdql_stewards_sample_media on r_detect_qnt_lmt_swap_stewards(sample_media) local parallel 4 nologging;
 --rollback drop index rdql_stewards_sample_media;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_site
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_SITE';
-create bitmap index rdql_stewards_site on r_detect_qnt_lmt_swap_stewards(site_id) parallel 4 nologging;
+create bitmap index rdql_stewards_site on r_detect_qnt_lmt_swap_stewards(site_id) local parallel 4 nologging;
 --rollback drop index rdql_stewards_site;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_site_type
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_SITE_TYPE';
-create bitmap index rdql_stewards_site_type on r_detect_qnt_lmt_swap_stewards(site_type) parallel 4 nologging;
+create bitmap index rdql_stewards_site_type on r_detect_qnt_lmt_swap_stewards(site_type) local parallel 4 nologging;
 --rollback drop index rdql_stewards_site_type;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_state
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_STATE';
-create bitmap index rdql_stewards_state on r_detect_qnt_lmt_swap_stewards(state_code) parallel 4 nologging;
+create bitmap index rdql_stewards_state on r_detect_qnt_lmt_swap_stewards(state_code) local parallel 4 nologging;
 --rollback drop index rdql_stewards_state;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_station
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_STATION';
-create bitmap index rdql_stewards_station on r_detect_qnt_lmt_swap_stewards(station_id) parallel 4 nologging;
+create bitmap index rdql_stewards_station on r_detect_qnt_lmt_swap_stewards(station_id) local parallel 4 nologging;
 --rollback drop index rdql_stewards_station;
 
 --changeset drsteini:WQP-1033.create.rdql_stewards_taxa_name
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from user_indexes where index_name = 'RDQL_STEWARDS_TAXA_NAME';
-create bitmap index rdql_stewards_taxa_name on r_detect_qnt_lmt_swap_stewards(sample_tissue_taxonomic_name) parallel 4 nologging;
+create bitmap index rdql_stewards_taxa_name on r_detect_qnt_lmt_swap_stewards(sample_tissue_taxonomic_name) local parallel 4 nologging;
 --rollback drop index rdql_stewards_taxa_name;
