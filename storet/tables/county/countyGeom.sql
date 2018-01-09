086200849b91d1019af21eb49b92e1b6758f06de
Stanley Kaymen <skaymen@usgs.gov>
2018-01-09 09:36:51 -0600

    add countygeom table

---
+++ storet/tables/county/countyGeom.sql
@@ -0,0 +1,10 @@
+create table countyGeom
+(fips_county_code                 varchar2(3 char)
+,county_name                      varchar2(256 char)
+,geom                             sdo_geometry
+,fips_state_code                  varchar2(4 char)
+,state_postal_code                varchar2(2 char)
+,state_name                       varchar2(256 char)
+,country_code                     varchar2(2 char)
+,country_name                     varchar2(256 char)
+);
