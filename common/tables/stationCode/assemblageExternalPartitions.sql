120b3ef96f42bd40338cc9052c1027365c7fbbfd
Stanley Kaymen <skaymen@usgs.gov>
2018-01-12 11:12:50 -0600

    add stationCode folders and code to all partitions

--- common/tables/stationCode/assemblageExternalPartitions.sql
+++
@@ -1,7 +0,0 @@
-alter table assemblage split partition assemblage_garbage into
-(partition assemblage_stewards values less than (2)
-,partition assemblage_nwis values less than (3)
-,partition assemblage_storet values less than (4)
-,partition assemblage_biodata values less than (5)
-,partition assemblage_garbage
-);
--- storet/tables/stationCode/stationCode.sql
+++ storet/tables/stationCode/stationCode.sql
@@ -1,10 +1,7 @@
-create table station_code
+create table station_code_swap_storet
 (data_source_id                 number
 ,code_value                     varchar2(500 char)
 ,description                    varchar2(4000 char)
 ,organization                   varchar2(4000 char)
 ,text                           varchar2(4000 char)
-) parallel 4 compress pctfree 0 nologging cache
-partition by range (data_source_id)
-(partition station_code_garbage values less than (maxvalue)
-);
+) parallel 4 compress pctfree 0 nologging cache;
