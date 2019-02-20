614866577ccad519e691bed59847cef65a527de3
David Steinich <drsteini@contractor.usgs.gov>
2019-02-20 12:02:24 -0600

    continue on WQP-1370

--- liquibase/dbInit/1_run_liquibase.sh
+++ liquibase/dbInit/1_run_liquibase.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # postgres to postgres db scripts
-${LIQUIBASE_HOME}/liquibase \
-	--classpath=${LIQUIBASE_HOME}/lib/${JDBC_JAR} \
-	--changeLogFile=${LIQUIBASE_WORKSPACE}/postgres/postgres/changeLog.yml \
+$LIQUIBASE_HOME/liquibase \
+	--classpath=$LIQUIBASE_HOME/lib/$JDBC_JAR \
+	--changeLogFile=$LIQUIBASE_WORKSPACE/postgres/postgres/changeLog.yml \
 	--driver=org.postgresql.Driver \
-	--url=jdbc:postgresql://${DATABASE_ADDRESS}:5432/postgres \
+	--url=jdbc:postgresql://$DATABASE_ADDRESS:5432/postgres \
 	--username=postgres \
-	--password=${POSTGRES_PASSWORD} \
-	--contexts=${CONTEXTS} \
+	--password=$POSTGRES_PASSWORD \
+	--contexts=$CONTEXTS \
 	--logLevel=debug \
 	update \
-	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-	-DDATABASE_NAME=${DATABASE_NAME} \
-	-DdataOwner=${DATA_OWNER} \
-	-DDATA_OWNER_PASSWORD=${DATA_OWNER_PASSWORD} \
-	-DschemaName=${SCHEMA_NAME} \
-	-DreadOnlyUser=${READ_ONLY_USER} \
-	-DREAD_ONLY_USER_PASSWORD=${READ_ONLY_USER_PASSWORD}
+	-DPOSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+	-DDATABASE_NAME=$DATABASE_NAME \
+	-DdataOwner=$DATA_OWNER \
+	-DDATA_OWNER_PASSWORD=$DATA_OWNER_PASSWORD \
+	-DschemaName=$SCHEMA_NAME \
+	-DreadOnlyUser=$READ_ONLY_USER \
+	-DREAD_ONLY_USER_PASSWORD=$READ_ONLY_USER_PASSWORD
 
 # postgres to wqp db scripts
 ${LIQUIBASE_HOME}/liquibase \
