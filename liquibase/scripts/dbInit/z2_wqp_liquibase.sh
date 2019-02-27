8325287fbc503852b40b9ef5ff72fc5ac515053f
David Steinich <drsteini@contractor.usgs.gov>
2019-02-27 14:19:29 -0600

    adjust environment names

--- liquibase/scripts/dbInit/z2_wqp_liquibase.sh
+++ liquibase/scripts/dbInit/z2_wqp_liquibase.sh
@@ -5,11 +5,11 @@ ${LIQUIBASE_HOME}/liquibase \
 	--changeLogFile=${LIQUIBASE_WORKSPACE}/wqp/changeLog.yml \
 	--driver=org.postgresql.Driver \
 	--url=jdbc:postgresql://${DATABASE_ADDRESS}:5432/${DATABASE_NAME} \
-	--username=${DATA_OWNER} \
-	--password=${DATA_OWNER_PASSWORD} \
+	--username=${WQP_DB_DATA_OWNER_USERNAME} \
+	--password=${WQP_DB_DATA_OWNER_PASSWORD} \
 	--contexts=${CONTEXTS} \
 	--logLevel=debug \
 	update \
-	-DdataOwner=${DATA_OWNER} \
+	-DdataOwner=${WQP_DB_DATA_OWNER_USERNAME} \
 	-DschemaName=${SCHEMA_NAME} \
-	-DreadOnlyUser=${READ_ONLY_USER}
+	-DreadOnlyUser=${WQP_DB_READ_ONLY_USERNAME}
